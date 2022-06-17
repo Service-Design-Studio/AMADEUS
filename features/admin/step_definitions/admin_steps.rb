646234b78f40c36ad10ca08405b561f0cdb220b7
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-06-17 11:46:10 +0800

    refactor: DRYer Cucumber scenario

--- features/admin/step_definitions/admin_steps.rb
+++ features/admin/step_definitions/admin_steps.rb
@@ -9,3 +9,27 @@ end
 And(/^I am trying to set articles from the upload page$/) do
   pending
 end
+
+# Upload feature
+## View upload page
+Then(/^I should see a "([^"]*)" button$/) do |button_name|
+  pending
+end
+
+And(/^I should see a warning text "([^"]*)"$/) do |warning_text|
+  pending
+end
+
+And(/^I should see a "([^"]*)" field$/) do |field_text|
+  pending
+end
+
+## Upload functionality
+
+# When(/^I click "([^"]*)" button to select the "([^"]*)" zipped folder$/) do |button_name, folder_name|
+#   pending
+# end
+
+When(/^I click "([^"]*)" button to select the "([^"]*)" (zipped folder|file)$/) do |arg1, arg2|
+  pending
+end
