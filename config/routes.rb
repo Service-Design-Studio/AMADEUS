6b16872ce337fed2cc42eae5d69c57fa733c8e21
gnnort <gnnort@DESKTOP-VUSQFBT.localdomain>
2022-06-19 17:02:06 +0800

    feat: upload zip feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # get 'upload_file/upload'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :uploads
 
   scope :admin do
     resources :articles, only: [:index, :update]
