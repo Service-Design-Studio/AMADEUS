29dc1e3d0e72056796ccf830fd1d80a9e9583889
TNBL265 <tran.nguyen.bao.long.2018@vjc.sg>
2022-06-21 16:28:10 +0800

    feat: add logout for capybara

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -1,9 +1,13 @@
 include Warden::Test::Helpers
 
 module KnowsUser
-  def login(email, password)
+  def capybara_login(email, password)
     user = User.create!(email: email, password: password)
     login_as(user, :scope => :user)
+  end
+
+  def capybara_logout
+    logout()
   end
 end
 World(KnowsUser)
