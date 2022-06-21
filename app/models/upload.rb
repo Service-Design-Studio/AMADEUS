30ea855becdf1d5b564d05c3e61c5ad7f2e2d27f
gnnort <tron_piggy@yahoo.com.sg>
2022-06-22 00:11:48 +0800

    bugfix: unzip file feature now handles nested directories

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,15 @@
 class Upload < ApplicationRecord
     has_one_attached :file
+    validate :validate_attachment_filetype
+
+
+    private
+
+    def validate_attachment_filetype
+        return unless file.attached?
+
+        unless file.content_type.in?(%w[application/pdf])
+            errors.add(:file, 'must be a ZIP!')
+        end
+    end
 end
