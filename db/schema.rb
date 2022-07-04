fbe34f07b651329ecedeb03228fca9d4bd219231
gnnort <tron_piggy@yahoo.com.sg>
2022-07-04 21:00:02 +0800

    chore: change Upload Database to Uploaded Files

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_111720) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    #t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
