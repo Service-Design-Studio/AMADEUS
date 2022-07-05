a3e2ddc45b102c3e039b86975f27f762f8e282ba
gnnort <tron_piggy@yahoo.com.sg>
2022-07-05 15:37:15 +0800

    feat: extract title from uploaded PDF file

--- Gemfile
+++ Gemfile
@@ -58,6 +58,9 @@ gem 'activeadmin'
 # Unzip
 gem 'rubyzip'
 
+# Parsing PDFs
+gem 'pdf-reader'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,30 +1,39 @@
 require 'zip'
+require 'pdf-reader'
+
 class Upload < ApplicationRecord
-    has_one_attached :file
-    validate :validate_attachment_filetype
+  has_one_attached :file
+  validate :validate_attachment_filetype
 
 
     private
 
     def validate_attachment_filetype
-        return unless file.attached?
+      return unless file.attached?
 
-        unless file.content_type.in?(%w[application/pdf])
-            errors.add(:file, 'must be a ZIP!')
-        end
+      unless file.content_type.in?(%w[application/pdf])
+        errors.add(:file, 'must be a ZIP!')
+      end
     end
 
     def self.unzip_file(file, params)
-        Zip::File.open(file) do |zipfile|
-            zipfile.each do |entry|
-              if entry.file?
-                new_upload = Upload.new()
-                new_upload.file.attach(io: StringIO.new(entry.get_input_stream.read), filename: entry.name)
-                new_upload.title = params[:upload][:title]
-                new_upload.save
-              end
-            end
+      Zip::File.open(file) do |zipfile|
+        zipfile.each do |entry|
+          if entry.file?
+            new_upload = Upload.new()
+            new_upload.file.attach(io: StringIO.new(entry.get_input_stream.read), filename: entry.name)
+            new_upload.title = get_pdf_title(entry)
+            new_upload.save
+          end
         end
+      end
     end
+
+    def self.get_pdf_title(pdf)
+      reader = PDF::Reader.new(StringIO.new(pdf.get_input_stream.read))
+      first_page = reader.pages[0].text.split("\n")
+      title = first_page[0]
+    end
+
 
 end
