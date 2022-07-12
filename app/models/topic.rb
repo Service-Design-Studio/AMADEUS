5dfbe90377cf161eefd6184bef7a70664abe0463
Anteloped <ernestwy.lim@gmail.com>
2022-07-12 21:22:36 +0800

    updated topic validation no special symbols

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
   has_many :uploadlinks, dependent: :destroy
   has_many :uploads, through: :uploadlinks
-  validates :name, presence: true, uniqueness: true, allow_blank: false
+  validates :name, presence: true, uniqueness: true
+  validates :name, :format => { :with => /\A[a-zA-Z0-9_]+\z/}
 end
