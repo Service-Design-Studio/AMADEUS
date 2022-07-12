7d27ebe453dcde02429ba0700ba5852f9a9567c4
Anteloped <ernestwy.lim@gmail.com>
2022-07-12 23:11:08 +0800

    Modified topic model, flash messages for topic and html

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ApplicationRecord
   has_many :uploadlinks, dependent: :destroy
   has_many :uploads, through: :uploadlinks
-  validates :name, presence: true, uniqueness: true
-  validates :name, :format => { :with => /\A[a-zA-Z0-9_]+\z/}
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, :format => { :with => /[a-zA-Z0-9_\s\/]/ }
 end
