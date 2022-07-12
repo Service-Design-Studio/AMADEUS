613946f2f1b14b31ad4bf86ba37dca68859cdaa4
Anteloped <ernestwy.lim@gmail.com>
2022-07-12 20:03:42 +0800

    fix: blanks not allowed, and only unique topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ApplicationRecord
   has_many :uploadlinks, dependent: :destroy
   has_many :uploads, through: :uploadlinks
+  validates :name, presence: true, uniqueness: true, allow_blank: false
 end
