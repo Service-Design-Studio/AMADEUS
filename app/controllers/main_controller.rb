30b0d69ea7dd73816e17b375ddf77ddb68da65c9
Wang Zhao <081101wangzhao@gmail.com>
2022-07-20 19:23:07 +0800

    [in progress] homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,13 @@
 class MainController < ApplicationController
     def index
-        # TODO: Different navbar for admin and non-admin users
+        # sort all categories by their name in ascending order
+        @categories = Category.all.sort_by { |category| category.name }
+        
+        @category = @categories.first
+    end
+
+    private
+    def flash_message
+        FlashString::MainString
     end
 end
