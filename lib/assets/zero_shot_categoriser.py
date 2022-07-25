f627fa498af1b77c83581d675b0e5c566251ec69
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-07-25 08:11:54 +0800

    feat: local run for ZeroShotCategoriser

--- lib/assets/zero_shot_categoriser.py
+++ lib/assets/zero_shot_categoriser.py
@@ -17,4 +17,4 @@ def getCategory():
     return jsonify({"data": {"category": top_category}})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
