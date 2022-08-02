e94cf0c12643bd34459c0814085bc36907bbc05b
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-08-02 21:05:12 +0800

    chore: simplify nltk

--- lib/assets/nltk_model.py
+++ lib/assets/nltk_model.py
@@ -1,22 +1,24 @@
 # All credits to Timothy W. https://github.com/weetimo
 import random
 import time
+import string
 
 print("Finish setup")
-
 
 def categoriser():
     categories = ['Tank', 'Artillery', 'UAV', 'Fighter Aircraft', 'Helicopter', 'Missile', 'MANPAD', 'Infrastructure']
     return random.choice(categories)
 
-
 def tagger():
-    tags_dict = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5 }
+    tags_dict = {}
+    for i in range(5):
+        key = random.choice(string.ascii_lowercase)
+        val = random.randint(0, 100)
+        tags_dict[key] = val
     return tags_dict
 
 def summariser():
     return "This is a valid summary with more than 10 words and less than 100 words"
-
 
 def nltk_model(request):
     request_json = request.get_json()
