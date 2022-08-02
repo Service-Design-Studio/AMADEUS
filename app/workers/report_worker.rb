6eb7f69b4c6920c3085742cd0f770908e5c08381
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-08-02 09:35:19 +0800

    feat: Summarizer feature

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -5,10 +5,8 @@ class ReportWorker
     # This method that will be called when the worker is queued.
     def perform(zip_id, upload_id)
         if zip_id != ""
-            puts "SIDEKIQ WORKER UPZIPPING ZIP ID: #{zip_id}"
             ZipUpload.unzip_file(zip_id)
         elsif upload_id != ""
-            puts "SIDEKIQ WORKER RUNNING NLTK ON UPLOAD ID: #{upload_id}"
             Upload.run_nltk(upload_id)
         end
     end
