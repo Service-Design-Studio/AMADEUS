b89896a4fe3e0167cfab8a0eda9e1c1904c7648e
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-08-03 14:33:08 +0800

    fix: Cucumber for basic and summariser features

--- features/step_definitions/upload/upload_index_steps.rb
+++ features/step_definitions/upload/upload_index_steps.rb
@@ -5,7 +5,7 @@ When(/^I click edit article "([^"]*)"$/) do |article_name|
   expect(page).to have_current_path("/admin/uploads/#{id}/edit")
 end
 
-Then(/^I should be redirected to the article "([^"]*)" edit page$/) do |article_name|
+Then(/^I should be redirected to the edit page for the article "([^"]*)"$/) do |article_name|
   id = capybara_get_article_idx(article_name)
   expect(page).to have_current_path("/admin/uploads/#{id}/edit")
 end
