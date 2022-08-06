0ade97c8231c908447a458a0882392c10da36457
Wang Zhao <081101wangzhao@gmail.com>
2022-08-06 15:47:59 +0800

    fixed homepage UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def side_bar_visible()
+        # if url starts with /admin or /admin/
+        if request.path.start_with?("/admin")
+            return false
+        end
+        return true
+    end
+
 end
