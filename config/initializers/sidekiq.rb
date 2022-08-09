73ffe819f8a3ab02d90387b31bf1bf12dc2fc9a0
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-08-09 13:59:09 +0800

    chore: deploy config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 redis = { url: ENV.fetch('REDIS_URL', "redis://localhost:6379/1"), id: nil }
 Sidekiq.configure_server do |config|
   config.redis = redis
-  config.logger = nil
+  # config.logger = nil
 end
 Sidekiq.configure_client do |config|
   config.redis = redis
