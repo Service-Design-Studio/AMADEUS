033751cc25406c574b013944a5e92f3fbaf21f05
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-08-09 22:22:47 +0800

    chore: brute force test

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,4 @@
-redis = { url: ENV.fetch('REDIS_URL', "redis://localhost:6379/1"), id: nil,
-          password: Rails.application.credentials.redis_key }
+redis = { url: ENV.fetch('REDIS_URL', "redis://localhost:6379/1"), id: nil }
 Sidekiq.configure_server do |config|
   config.redis = redis
   # config.logger = nil
